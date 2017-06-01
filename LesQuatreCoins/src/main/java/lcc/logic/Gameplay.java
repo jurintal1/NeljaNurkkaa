ff0e63a29316b81f15de5c47679d8c4419e4bd01
hiihtoliitto <jussi.rintala@gmail.com>
2017-06-01 23:22:14 +0300

    checkstyle-korjaukset ja -raportti

--- LesQuatreCoins/src/main/java/lcc/logic/Gameplay.java
+++ LesQuatreCoins/src/main/java/lcc/logic/Gameplay.java
@@ -1,29 +1,23 @@
 package lcc.logic;
 
-public class Gameplay { 
-    
-    
-    
+public class Gameplay {
+
     public boolean move(Deck source, Deck destination) {
-        if (source == destination || source instanceof FoundationDeck 
+        if (source == destination || source instanceof FoundationDeck
                 || !destination.allowedToAdd(source.topCard())) {
             return false;
         }
         destination.addCard(source.takeCard());
-        return true; 
+        return true;
     }
-    
+
     public boolean move(SideDeck source, FoundationDeck destination) {
-        if (source.getSuit()==destination.getSuit()
+        if (source.getSuit() == destination.getSuit()
                 && destination.allowedToAdd(source.topCard())) {
             destination.addCard(source.takeCard());
             return true;
-        }        
-        return false; 
+        }
+        return false;
     }
-    
-    
-    
-    
 
 }
